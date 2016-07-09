f1874257a52a262ab5fe5cb595938499fc5e157b
respinozag <r.espinozagarrido@gmail.com>
2016-07-09 19:53:18 -0400

    banner

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -2,33 +2,21 @@
     <article id="art">
         <div class="wrapper">
             <h3 style="text-align: center;  letter-spacing: 5px; font-weight: bolder; color: black; margin-top: 20px; margin-bottom: 6%; font-weight: bolder;">Tienda de licores Kukarrox</h3>
-            <div class="row">
-                <div class="col-md-4">
-                    <!--                    class="textodeinicio-->
-                    <h4 style="  margin-left: 58px; color:black;">Lo más vendido</h4> 
-                    <h1 style="margin-left: 8px;" id="lomasvendido">six</h1> 
-                </div>
-                <div class="col-md-4">
-                    <h4 style="text-align: center; color:black;">Productos premium</h4> 
+       
 
-                    <h1 style="margin-left:70px;" id=premium></h1>
-                </div>
+            <div style="width: 98%">  
 
-                <div class="col-md-4">
-                    <h4 style="margin-left:170px ; color:black">Otros productos</h4> 
-
-                    <h1 id=otrosproductos>jonny</h1>
-                </div>
-
-
+                <marquee  scrolldelay="1" scrollamount="8" direction="left" loop="infinite" onmouseout="this.start()" onmouseover="this.stop()">
+                    <a ><img  title="$3990" alt="Promo 3" src="../../imagenes/pisco.jpg" height ="200px" width="250px"/></h1></a>  <a><img title="$3990" alt="Promo 2" src="../../imagenes/caja_corona.jpg" height ="200px" width="250px"/></a>  <a><img title="$3990" alt="Promo 1" src="../../imagenes/Bar-Premium.png" height ="200px" width="250px"/></a>
+                    
+                    
+                </marquee>
 
 
             </div>
-           
-                
-                 
-                <br/>
-                
+
+            <br/>
+
 
         </div>
 </div>
