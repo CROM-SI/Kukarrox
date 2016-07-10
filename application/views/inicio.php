a766b413fe4d0a8eb28c6d2449d74e3617a5a02e
respinozag <r.espinozagarrido@gmail.com>
2016-07-10 03:10:08 -0400

    fotos

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -1,22 +1,20 @@
 <div id="inicio">
     <article id="art">
         <div class="wrapper">
-            <h3 style="text-align: center;  letter-spacing: 5px; font-weight: bolder; color: black; margin-top: 20px; margin-bottom: 6%; font-weight: bolder;">Tienda de licores Kukarrox</h3>
-       
+            <h3 style="text-align: center;  font-family:sans-serif ; 
+                letter-spacing: 5px; font-weight: bolder; 
+                color: black; margin-top: 20px; margin-bottom: 6%; 
+                font-weight: bolder;">Promociones KUKARROX</h3>
+
 
             <div style="width: 98%">  
-
-                <marquee  scrolldelay="1" scrollamount="8" direction="left" loop="infinite" onmouseout="this.start()" onmouseover="this.stop()">
-                    <a ><img  title="$3990" alt="Promo 3" src="../../imagenes/pisco.jpg" height ="200px" width="250px"/></h1></a>  <a><img title="$3990" alt="Promo 2" src="../../imagenes/caja_corona.jpg" height ="200px" width="250px"/></a>  <a><img title="$3990" alt="Promo 1" src="../../imagenes/Bar-Premium.png" height ="200px" width="250px"/></a>
-                    
-                    
+                <marquee  scrolldelay="1" scrollamount="10" direction="left" loop="infinite" onmouseout="this.start()" onmouseover="this.stop()">
+                    <a ><img  title="Promo 1" alt="Promo 1" src="http://i.imgur.com/hiqjtez.jpg" height ="550px" width="450px"/></h1></a>  
+                    <a><img title="Promo 2" alt="Promo 2" src="http://i.imgur.com/mEAF1kW.jpg"  height ="550px" width="450px"/></a>  
+                    <a><img title="Promo 3" alt="Promo 3" src="http://i.imgur.com/QR0FfFT.jpg"  height ="550px" width="450px"/></a>
+                    <a><img title="Promo 4" alt="Promo 4" src="http://i.imgur.com/6yq9Oaf.jpg"  height ="550px" width="450px"/></a>
                 </marquee>
-
-
             </div>
-
-            <br/>
-
-
+            
         </div>
 </div>
