0804c630581ba228b3ea53f8f35ee30747bff0f3
respinozag <r.espinozagarrido@gmail.com>
2016-07-09 20:17:24 -0400

    aers

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -1,13 +1,15 @@
 <div id="inicio">
     <article id="art">
         <div class="wrapper">
-            <h3 style="text-align: center;  letter-spacing: 5px; font-weight: bolder; color: black; margin-top: 20px; margin-bottom: 6%; font-weight: bolder;">Tienda de licores Kukarrox</h3>
+            <h3 style="text-align: center;  letter-spacing: 5px; font-weight: bolder; color: black; margin-top: 20px; margin-bottom: 6%; font-weight: bolder;">Promociones Kukarrox</h3>
        
 
             <div style="width: 98%">  
 
                 <marquee  scrolldelay="1" scrollamount="8" direction="left" loop="infinite" onmouseout="this.start()" onmouseover="this.stop()">
-                    <a ><img  title="$3990" alt="Promo 3" src="../../imagenes/pisco.jpg" height ="200px" width="250px"/></h1></a>  <a><img title="$3990" alt="Promo 2" src="../../imagenes/caja_corona.jpg" height ="200px" width="250px"/></a>  <a><img title="$3990" alt="Promo 1" src="../../imagenes/Bar-Premium.png" height ="200px" width="250px"/></a>
+                    <a ><img  title="$3990" alt="Promo 3" src="http://www.copeteadomicilio.cl/images/promocion%20pisco%20mistral%20-%20sprite%20-hielo318x318.jpg" height ="200px" width="180px"/></h1></a>  
+                    <a><img title="$3990" alt="Promo 2" src="../../imagenes/caja_corona.jpg" height ="200px" width="250px"/></a>  
+                    <a><img title="$3990" alt="Promo 1" src="../../imagenes/Bar-Premium.png" height ="200px" width="250px"/></a>
                     
                     
                 </marquee>
