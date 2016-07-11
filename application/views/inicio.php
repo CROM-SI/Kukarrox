b5380e8d4b0f5c23cace3050236b2f28e5f2fa5a
respinozag <r.espinozagarrido@gmail.com>
2016-07-10 22:43:21 -0400

    asd

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -9,10 +9,10 @@
 
             <div style="width: 98%">  
                 <marquee  scrolldelay="1" scrollamount="10" direction="left" loop="infinite" onmouseout="this.start()" onmouseover="this.stop()">
-                    <a><img title="Promo 1" alt="Promo 1" src="http://i.imgur.com/hiqjtez.jpg" height ="550px" width="450px" style="border-radius: 5%"/></h1></a>  
-                    <a><img title="Promo 2" alt="Promo 2" src="http://i.imgur.com/mEAF1kW.jpg"  height ="550px" width="450px" style="border-radius: 5%"/></a>  
-                    <a><img title="Promo 3" alt="Promo 3" src="http://i.imgur.com/QR0FfFT.jpg"  height ="550px" width="450px" style="border-radius: 5%"/></a>
-                    <a><img title="Promo 4" alt="Promo 4" src="http://i.imgur.com/6yq9Oaf.jpg"  height ="550px" width="450px" style="border-radius: 5%"/></a>
+                    <a><img title="Promo 1" alt="Promo 1" src="http://i.imgur.com/hiqjtez.jpg" height ="450px" width="350px" style="border-radius: 5%"/></h1></a>  
+                    <a><img title="Promo 2" alt="Promo 2" src="http://i.imgur.com/mEAF1kW.jpg"  height ="450px" width="350px" style="border-radius: 5%"/></a>  
+                    <a><img title="Promo 3" alt="Promo 3" src="http://i.imgur.com/QR0FfFT.jpg"  height ="450px" width="350px" style="border-radius: 5%"/></a>
+                    <a><img title="Promo 4" alt="Promo 4" src="http://i.imgur.com/6yq9Oaf.jpg"  height ="450px" width="350px" style="border-radius: 5%"/></a>
                 </marquee>
             </div>
             
