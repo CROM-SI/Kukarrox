b0f2e12d4557ff4cdb1f975ea8d6a9ec4ed4d7d3
matiasgon <oscargonzalezrojas@MacBook-Pro-de-Oscar.local>
2016-07-12 00:17:14 -0300

    go

--- application/controllers/Controlador.php
+++ application/controllers/Controlador.php
@@ -508,4 +508,101 @@ class Controlador extends CI_Controller {
         $this->load->view("verPedidos",$datos);
     }
     
+    function cargarPdf(){
+        $this->load->view("vista_pdf");
+    }
+
+
+    
+    public function generar() {
+        $this->load->library('Pdf');
+        $pdf = new Pdf('P', 'mm', 'A4', true, 'UTF-8', false);
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetAuthor('');
+        $pdf->SetTitle('');
+        $pdf->SetSubject('');
+        $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+ 
+// datos por defecto de cabecera, se pueden modificar en el archivo tcpdf_config_alt.php de libraries/config
+        $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE . ' ', PDF_HEADER_STRING, array(0, 64, 255), array(0, 64, 128));
+        $pdf->setFooterData($tc = array(0, 64, 0), $lc = array(0, 64, 128));
+ 
+// datos por defecto de cabecera, se pueden modificar en el archivo tcpdf_config.php de libraries/config
+        $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+        $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+ 
+// se pueden modificar en el archivo tcpdf_config.php de libraries/config
+        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+ 
+// se pueden modificar en el archivo tcpdf_config.php de libraries/config
+        $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+ 
+// se pueden modificar en el archivo tcpdf_config.php de libraries/config
+        $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+ 
+//relación utilizada para ajustar la conversión de los píxeles
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+ 
+ 
+// ---------------------------------------------------------
+// establecer el modo de fuente por defecto
+        $pdf->setFontSubsetting(true);
+ 
+// Establecer el tipo de letra
+ 
+//Si tienes que imprimir carácteres ASCII estándar, puede utilizar las fuentes básicas como
+// Helvetica para reducir el tamaño del archivo.
+        $pdf->SetFont('freemono', '', 14, '', true);
+ 
+// Añadir una página
+// Este método tiene varias opciones, consulta la documentación para más información.
+        $pdf->AddPage();
+ 
+//fijar efecto de sombra en el texto
+        $pdf->setTextShadow(array('enabled' => true, 'depth_w' => 0.2, 'depth_h' => 0.2, 'color' => array(196, 196, 196), 'opacity' => 1, 'blend_mode' => 'Normal'));
+ 
+// Establecemos el contenido para imprimir
+        
+        $pedidos = $this->modelo->mostrarPedido();
+        
+        //preparamos y maquetamos el contenido a crear
+        $html = '';
+        $html .= "<style type=text/css>";
+        $html .= "th{color: #fff; font-weight: bold; background-color: #222}";
+        $html .= "h2{text-align: center;}";
+        $html .= "td{background-color: #AAC7E3; color: #fff}";
+        $html .= "table{font-size: 12px; margin-top: 8%; width: 480px; text-align: center;  
+                 border-collapse: collapse; margin-left: 50%;}";
+        $html .= "</style>";
+        $html .= "<h2>Comprobante</h2>";
+        $html .= "<h3>Detalle del pedido</h3>";
+        $html .= "<table width='100%'>";
+        $html .= "<tr><th>Nombre producto</th><th>Cantidad</th><th>Precio</th></tr>";
+        
+        //provincias es la respuesta de la función getProvinciasSeleccionadas($provincia) del modelo
+        foreach ($pedidos->result() as $row){
+            $html .= "<tr>";
+                $html .= "<td>".$row->nombre_producto."</td>";
+                $html .= "<td>".$row->cantidad."</td>";
+                $html .= "<td>".$row->precio_por_unidad."</td>";
+            $html .= "</tr>";  
+                
+        }
+        $html .= "</table>";
+ 
+// Imprimimos el texto con writeHTMLCell()
+        $pdf->writeHTMLCell($w = 0, $h = 0, $x = '', $y = '', $html, $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+ 
+// ---------------------------------------------------------
+// Cerrar el documento PDF y preparamos la salida
+// Este método tiene varias opciones, consulte la documentación para más información.
+        $nombre_archivo = utf8_decode("Comprobante.pdf");
+        $pdf->Output($nombre_archivo, 'I');
+    }
+
+    
+    
+    
 }
