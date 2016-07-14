aaa4256bd964b9b15db8af92836e5237b32e5baf
respinozag <r.espinozagarrido@gmail.com>
2016-07-14 01:21:27 -0400

    a medias

--- application/views/listacarrito.php
+++ application/views/listacarrito.php
@@ -1,46 +1,51 @@
-<div id="content" style="padding-right: 23%; margin-top: 5%">
-<form method="POST" action="<?=base_url()?>Controlador/eliminarproC">
-    <table id="table"  border="2" width="500">
-        <caption>Mis Productos</caption>
-        <tr>
-            <th style="text-align:center"><h5>Producto</h5></th>
-            <th style="text-align:center"><h5>Precio</h5></th>
-            <th style="text-align:center"><h5>Cantidad</h5></th>
-            
-        </tr>  
-        <tr>
-            
-            <?php foreach ($carrito as $i){ ?>
-        
-                <tr>
-                <td> <?=$i->nombre_producto?> </td>
-               
-                <td> <?=$i->precio_por_unidad?></td>
-               
-                <td> <?=$i->cantidad?></td>
-              
-                <td> <input type="text" hidden="true"  name=""  value="<?=$i->id_carrito?>"/></td>
-                <td><button id="btneliminarC" name="id" value="<?=$i->id_carrito?>" class="btn btn-succes"  >Eliminar</button></td>
-                         
-                </tr>
-       
-            <?php }?>
-                 
-        </tr>  
-    </table>
-    <br> <br>
-    <div>
-        <center>
-            
-               <a href="<?=base_url()?>Controlador/generar" id="generarPedido"  class="btn btn-succes" 
-                  style="background-color: red; color: black; margin-left: 135%" 
-                  >Generar Pedido</a>
- 
-        </center>
-        
-        <a href="<?=base_url()?>Controlador/cargarPedido" class="btn" id="volLista">Volver</a>
-    </div>
+<div id="content" style=" margin-top: 5%">
 
- </form>
-   
+
+    <form method="POST" action="<?= base_url() ?>Controlador/eliminarproC">
+
+
+
+        <table id="table"  border="2" width="500" style="margin-left: 19%;margin-bottom: 10%;margin-top: -0%">
+            <caption>Mis Productos</caption>
+            <tr>
+                <th style="text-align:center"><h5>Producto</h5></th>
+                <th style="text-align:center"><h5>Precio</h5></th>
+                <th style="text-align:center"><h5>Cantidad</h5></th>
+            </tr> 
+
+            <tr>
+                <?php foreach ($carrito as $i) { ?>
+                <tr>
+                    <td> <?= $i->nombre_producto ?> </td>
+                    <td> <?= $i->precio_por_unidad ?></td>
+                    <td> <?= $i->cantidad ?></td>
+                    <td> <input type="text" hidden="true"  name=""  value="<?= $i->id_carrito ?>"/></td>
+                    <td><button id="btneliminarC" name="id" value="<?= $i->id_carrito ?>" class="btn btn-succes"  >Eliminar</button></td>
+                </tr>
+            <?php } ?>
+
+            </tr>  
+        </table>
+
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+
+                <button class="nose" >    <a href="<?= base_url() ?>Controlador/generar" id="generarPedido"  >Generar Pedido</a>
+                </button>
+
+            </div>
+            <div class="col-md-4">
+
+
+                <a href="<?= base_url() ?>Controlador/cargarPedido" class="btn" id="volLista">Volver</a>
+
+            </div>
+
+
+
+        </div>
+
+    </form>
+
 </div>
